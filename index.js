5e671aeaa2ab80e9203920e67df2701d69cbeba2
Asiandayboy <russellcheeseday@gmail.com>
2024-10-15 19:58:17 -0400

    Add contact form

--- index.js
+++ index.js
@@ -11,3 +11,35 @@ function toggleNavbar() {
 
 
 hamburgerIconSVG.addEventListener("click", toggleNavbar)
+
+
+
+
+function showEmailForm() {
+    const emailFormWrapper = document.querySelector(".email-form-wrapper")
+
+    setTimeout(() => {
+        if (emailFormWrapper.classList.contains("show-email-form")) {
+            emailFormWrapper.scrollIntoView({ behavior: "smooth" })
+        }
+    }, 301)
+
+    emailFormWrapper.classList.toggle("show-email-form")
+}
+
+
+
+
+const emailForm = document.getElementById("email-contact-form")
+
+emailForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm("service_nwlmgcd", "template_2vy37ok", emailForm).then(
+        (response) => {
+            alert("Email successfully sent.")
+        }, 
+        (error) => {
+            alert("Failed to send email message. Try again. If this problem persists, please send your message through your email client instead.")
+        })
+})
